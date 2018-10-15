cd662648d1afc62c9b2c230284bfbb2039583781
ChristophSchranz <christoph.schranz@salzburgresearch.at>
2018-10-15 13:20:36 +0200

    Add show service bash

--- show-operator-dashboard.sh
+++ show-operator-dashboard.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-echo "Printing 'docker service ls | grep op:"
-docker service ls | grep op
+echo "Printing 'docker service ls | grep op_:"
+docker service ls | grep op_
 echo ""
-echo "Printing 'docker stack ps op':"
-docker stack ps op
+echo "Printing 'docker stack ps op_':"
+docker stack ps op_
